9024a1debcc036e198dc7ae1de466435f2c404c3
AposVaki <153004990+AposVaki@users.noreply.github.com>
2024-01-07 13:13:03 +0200

    Delete src/gr/aueb/cf/ch12/bankapp/model/package-info.java

--- src/gr/aueb/cf/ch12/bankapp/model/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package includes the following model classes:
- * <br>
- * {@link gr.aueb.cf.ch12.bankapp.model.JointAccount}
- * {@link gr.aueb.cf.ch12.bankapp.model.OverDraftAccount}
- */
-package gr.aueb.cf.ch12.bankapp.model;
